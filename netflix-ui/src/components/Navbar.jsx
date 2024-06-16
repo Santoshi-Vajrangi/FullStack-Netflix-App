0bd8fd29c3d22afe97591a4889f0d68f37bc70b7
Santoshi-Vajrangi <143388470+Santoshi-Vajrangi@users.noreply.github.com>
2024-06-17 00:25:45 +0530

    Update Navbar.jsx

--- netflix-ui/src/components/Navbar.jsx
+++ netflix-ui/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { signOut } from "firebase/auth";
+ import { signOut } from "firebase/auth";
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
