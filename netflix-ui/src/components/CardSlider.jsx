02eaf5b9d39c5146609e9d3217d89d14fc029507
Santoshi-Vajrangi <143388470+Santoshi-Vajrangi@users.noreply.github.com>
2024-06-17 00:24:51 +0530

    Update CardSlider.jsx

--- netflix-ui/src/components/CardSlider.jsx
+++ netflix-ui/src/components/CardSlider.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+ import React, { useRef, useState } from "react";
 import styled from "styled-components";
 import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
 import Card from "./Card";
