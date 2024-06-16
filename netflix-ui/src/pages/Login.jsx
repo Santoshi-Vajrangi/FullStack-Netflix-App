c2827bd4da05167fe6f9ad0b2cf881503ada2518
Santoshi-Vajrangi <143388470+Santoshi-Vajrangi@users.noreply.github.com>
2024-06-17 00:09:37 +0530

    Update Login.jsx

--- netflix-ui/src/pages/Login.jsx
+++ netflix-ui/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+ import React, { useState } from "react";
 import styled from "styled-components";
 import logo from "../assets/logo.png";
 import background from "../assets/login.jpg";
