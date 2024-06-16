85d6f7028496c94fad434584995cf2370ab9db40
Santoshi-Vajrangi <143388470+Santoshi-Vajrangi@users.noreply.github.com>
2024-06-17 00:15:06 +0530

    Update TVShows.jsx

--- netflix-ui/src/pages/TVShows.jsx
+++ netflix-ui/src/pages/TVShows.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+ import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import Navbar from "../components/Navbar";
 import CardSlider from "../components/CardSlider";
